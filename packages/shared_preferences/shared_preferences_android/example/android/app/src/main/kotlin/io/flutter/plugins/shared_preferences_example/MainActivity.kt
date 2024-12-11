5fc634848fe198cd9e1e166e3ce904c953ce3676
tarrinneal <tarrinneal@gmail.com>
2024-12-11 14:59:16 -0800

    copying pigeon again

--- packages/shared_preferences/shared_preferences_android/example/android/app/src/main/kotlin/io/flutter/plugins/shared_preferences_example/MainActivity.kt
+++ packages/shared_preferences/shared_preferences_android/example/android/app/src/main/kotlin/io/flutter/plugins/shared_preferences_example/MainActivity.kt
@@ -6,4 +6,4 @@ package io.flutter.plugins.shared_preferences_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {}
