235f0022de66ca7e7c904094ee5c0aeda6a326e3
tarrinneal <tarrinneal@gmail.com>
2024-12-11 12:24:51 -0800

    @JvmField

--- packages/shared_preferences/shared_preferences_android/example/android/app/src/androidTest/kotlin/io/flutter/plugins/shared_preferences_example/MainActivityTest.kt
+++ packages/shared_preferences/shared_preferences_android/example/android/app/src/androidTest/kotlin/io/flutter/plugins/shared_preferences_example/MainActivityTest.kt
@@ -13,5 +13,5 @@ import org.junit.runner.RunWith
 @DartIntegrationTest
 @RunWith(FlutterTestRunner::class)
 class MainActivityTest {
-  @Rule var rule: ActivityTestRule<FlutterActivity> = ActivityTestRule(FlutterActivity::class.java)
+  @Rule @JvmField var rule: ActivityTestRule<FlutterActivity> = ActivityTestRule(FlutterActivity::class.java)
 }
